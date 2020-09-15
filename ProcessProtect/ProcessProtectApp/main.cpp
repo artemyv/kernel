655148b53c2716060ff83d0f55da68c087b92245
Artemy Vysotsky <artemyv@checkpoint.com>
2020-09-15 09:59:54 +0300

    ProcessProtect driver implementation

--- ProcessProtect/ProcessProtectApp/main.cpp
+++ ProcessProtect/ProcessProtectApp/main.cpp
@@ -2,13 +2,100 @@
 #include <string>
 #include <Windows.h>
 #include "../ProcessProtectDriver/Common.h"
-int main()
+
+void Usage(const char* prog)
 {
+    std::cout << "Usage:\n"
+        << prog << " add <pid> [<pid2> ...]\n"
+        << prog << " remove <pid> [<pid2> ...]\n"
+        << prog << " clear\n";
+}
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        Usage(argv[0]);
+        return 0;
+    }
+    enum class Command
+    {
+        add,
+        remove,
+        clear
+    }cmd;
+    std::string command{ argv[1] };
+    if (command == "clear")
+    {
+        cmd = Command::clear;
+    }
+    else if (command == "add")
+    {
+        cmd = Command::add;
+    }
+    else if (command == "remove")
+    {
+        cmd = Command::remove;
+    }
+    else
+    {
+        Usage(argv[0]);
+        return 0;
+    }
+
+    if ((cmd == Command::add || cmd == Command::remove) && argc < 3)
+    {
+        Usage(argv[0]);
+        return 0;
+    }
+
     auto device = CreateFileW(LR"(\\.\ProcessProtect)", GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
     if (!device)
     {
         std::cerr << "Failed to oped driver " << GetLastError() << '\n';
         return 1;
+    }
+    DWORD bytesReturned;
+    if (cmd == Command::clear)
+    {
+        if (!DeviceIoControl(device,
+            (DWORD)IOCTL_PROCESS_PROTECT_CLEAR,
+            nullptr,
+            0,
+            nullptr,
+            0,
+            &bytesReturned,
+            nullptr))
+        {
+            std::cerr << "Failed to call DeviceIoControl " << GetLastError() << '\n';
+        }
+        else
+        {
+            std::cout << "Protected process list cleared\n";
+        }
+    }
+    else
+    {
+        for (int index = 2; index < argc; index++)
+        {
+            std::string pid = argv[index];
+            PPProcessInfoIn in{ std::stoi(pid) };
+            DWORD control = (cmd == Command::add) ? IOCTL_PROCESS_PROTECT_BY_PID : IOCTL_PROCESS_UNPROTECT_BY_PID;
+            if (!DeviceIoControl(device,
+                control,
+                &in,
+                (DWORD)sizeof(in),
+                nullptr,
+                0,
+                &bytesReturned,
+                nullptr))
+            {
+                std::cerr << "Failed to call DeviceIoControl " << GetLastError() << '\n';
+            }
+            else
+            {
+                std::cout << "PID " << pid << ' ' << ((cmd == Command::add)?"added":"removed") << "\n";
+            }
+        }
     }
 
     CloseHandle(device);
--- ProcessProtect/ProcessProtectDriver/main.cpp
+++ ProcessProtect/ProcessProtectDriver/main.cpp
@@ -12,10 +12,104 @@ DRIVER_DISPATCH ProcessProtectCreateClose;
 _Dispatch_type_(IRP_MJ_DEVICE_CONTROL)
 DRIVER_DISPATCH ProcessProtectDeviceControl;
 
+OB_PREOP_CALLBACK_STATUS PobPreOperationCallback(
+    PVOID RegistrationContext,
+    POB_PRE_OPERATION_INFORMATION OperationInformation
+);
+
 #define LINK_NAME L"\\??\\ProcessProtect"
 
 #define FUNCTION_TYPE_FROM_CTL_CODE(ctrlCode)     (((ULONG)(ctrlCode & 0x3FFC)) >> 2)
+#define CB_PROCESS_TERMINATE 0x0001
 
+
+struct Protected
+{
+    HANDLE pid;
+};
+
+constexpr size_t max_len = 1024;
+struct Data
+{
+    Protected pids[max_len];
+    size_t len;
+    FAST_MUTEX m;
+
+    PVOID pCBRegistrationHandle = NULL;
+
+    bool IsProtectedProcess(POB_PRE_OPERATION_INFORMATION OperationInformation) 
+    { 
+        if (OperationInformation->ObjectType != *PsProcessType)
+            return false;
+
+        auto process = (PEPROCESS)OperationInformation->Object;
+        HANDLE h = PsGetProcessId(process);
+        bool found = false;
+        ExAcquireFastMutex(&m);
+        for (size_t index = 0; index < len; ++index)
+        {
+            if (h == pids[index].pid)
+            {
+                found = true;
+                break;
+            }
+        }
+        ExReleaseFastMutex(&m);
+        return found;
+    }
+
+    NTSTATUS clear()
+    {
+        ExAcquireFastMutex(&m);
+        len = 0;
+        ExReleaseFastMutex(&m);
+        return STATUS_SUCCESS;
+    }
+    NTSTATUS unprotect(HANDLE pid)
+    {
+        NTSTATUS result = STATUS_NOT_FOUND;
+        ExAcquireFastMutex(&m);
+        for (size_t index = 0; index < len; ++index)
+        {
+            if (pid == pids[index].pid)
+            {
+                pids[index] = pids[len -1];
+                len--;
+                result = STATUS_SUCCESS;
+                break;
+            }
+        }
+        ExReleaseFastMutex(&m);
+        return result;
+    }
+    NTSTATUS protect(HANDLE pid)
+    {
+        NTSTATUS result = STATUS_SUCCESS;
+        ExAcquireFastMutex(&m);
+        for (size_t index = 0; index < len; ++index)
+        {
+            if (pid == pids[index].pid)
+            {
+                result = STATUS_ALREADY_REGISTERED;
+                break;
+            }
+        }
+        if (result == STATUS_SUCCESS)
+        {
+            if (len < max_len)
+            {
+                pids[len].pid = pid;
+                ++len;
+            }
+            else
+            {
+                result = STATUS_INSUFFICIENT_RESOURCES;
+            }
+        }
+        ExReleaseFastMutex(&m);
+        return result;
+    }
+};
 _Use_decl_annotations_
 extern "C"  NTSTATUS
 DriverEntry(
@@ -37,9 +131,15 @@ DriverEntry(
         {
             if (!NT_SUCCESS(status))
             {
-                if(deviceObject)
+                if (deviceObject)
+                {
+                    auto data = (Data*)deviceObject->DeviceExtension;
+                    if (data && data->pCBRegistrationHandle)
+                    {
+                        ObUnRegisterCallbacks(data->pCBRegistrationHandle);
+                    }
                     IoDeleteDevice(deviceObject);
-
+                }
                 if (SymbolicLinkCreated)
                 {
                     UNICODE_STRING  ntWin32NameString = RTL_CONSTANT_STRING(LINK_NAME);
@@ -50,12 +150,15 @@ DriverEntry(
 
     } state;
 
-    state.status = IoCreateDevice(DriverObject, 0, &ntUnicodeString, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &state.deviceObject);
+    state.status = IoCreateDevice(DriverObject, sizeof(Data), &ntUnicodeString, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &state.deviceObject);
     if(!NT_SUCCESS(state.status))
     {
         KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "ERROR ProcessProtect.sys: Couldn't create Device 0x%x\r\n", state.status));
         return state.status;
     }
+    auto data = (Data*)state.deviceObject->DeviceExtension;
+    ExInitializeFastMutex(&data->m);
+    data->len = 0;
 
     UNICODE_STRING  ntWin32NameString = RTL_CONSTANT_STRING(LINK_NAME);
 
@@ -67,6 +170,40 @@ DriverEntry(
         // Delete everything that this routine has allocated.
         //
         KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "ERROR ProcessProtect.sys: Couldn't create symbolic link 0x%x\r\n", state.status));
+        return state.status;
+    }
+    state.SymbolicLinkCreated = true;
+
+
+    // Setup the Ob Registration calls
+    //  The following are for setting up callbacks for Process and Thread filtering
+
+    OB_CALLBACK_REGISTRATION  CBObRegistration = { 0 };
+    OB_OPERATION_REGISTRATION CBOperationRegistrations[1] = { { 0 } };
+    UNICODE_STRING CBAltitude = { 0 };
+
+    CBOperationRegistrations[0].ObjectType = PsProcessType;
+    CBOperationRegistrations[0].Operations |= OB_OPERATION_HANDLE_CREATE;
+    CBOperationRegistrations[0].Operations |= OB_OPERATION_HANDLE_DUPLICATE;
+    CBOperationRegistrations[0].PreOperation = PobPreOperationCallback;
+    CBOperationRegistrations[0].PostOperation = nullptr;
+
+    RtlInitUnicodeString(&CBAltitude, L"1000");
+
+    CBObRegistration.Version = OB_FLT_REGISTRATION_VERSION;
+    CBObRegistration.OperationRegistrationCount = 1;
+    CBObRegistration.Altitude = CBAltitude;
+    CBObRegistration.RegistrationContext = data;
+    CBObRegistration.OperationRegistration = CBOperationRegistrations;
+
+
+    state.status = ObRegisterCallbacks(
+        &CBObRegistration,
+        &data->pCBRegistrationHandle       // save the registration handle to remove callbacks later
+    );
+
+    if (!NT_SUCCESS(state.status)) {
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "ERROR ProcessProtect.sys: installing OB callbacks failed  status 0x%x\n", state.status));
         return state.status;
     }
 
@@ -93,6 +230,12 @@ void ProcessProtectUnload(
     //
 
     IoDeleteSymbolicLink(&ntWin32NameString);
+    auto data = (Data*)deviceObject->DeviceExtension;
+    if (data && data->pCBRegistrationHandle)
+    {
+        ObUnRegisterCallbacks(data->pCBRegistrationHandle);
+    }
+
     IoDeleteDevice(deviceObject);
 }
 
@@ -165,23 +308,79 @@ Return Value:
 {
     NTSTATUS            ntStatus = STATUS_INVALID_DEVICE_REQUEST;
 
-    UNREFERENCED_PARAMETER(DeviceObject);
-
     PAGED_CODE();
 
     PIO_STACK_LOCATION irpSp = IoGetCurrentIrpStackLocation(Irp);
     auto& dioc = irpSp->Parameters.DeviceIoControl;
     KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "INFO  ProcessProtect.sys: DeviceControl %u:0x%x\r\n", DEVICE_TYPE_FROM_CTL_CODE(dioc.IoControlCode), FUNCTION_TYPE_FROM_CTL_CODE(dioc.IoControlCode)));
+    auto data = (Data*)DeviceObject->DeviceExtension;
+    switch (dioc.IoControlCode)
+    {
+    case IOCTL_PROCESS_PROTECT_BY_PID :
+    case IOCTL_PROCESS_UNPROTECT_BY_PID:
+    {
+        if (dioc.Type3InputBuffer == nullptr)
+        {
+            ntStatus = STATUS_INVALID_PARAMETER;
+            KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "INFO  ProcessProtect.sys: Type3InputBuffer is null\r\n"));
+            break;
+        }
+        if (dioc.InputBufferLength < sizeof(PPProcessInfoIn))
+        {
+            ntStatus = STATUS_BUFFER_TOO_SMALL;
+            KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "INFO  ProcessProtect.sys: TypeInputBufferLegnth is %u less than %u\r\n",
+                unsigned(dioc.InputBufferLength), unsigned(sizeof(PPProcessInfoIn))));
+            break;
+        }
+        auto dataIn = (PPProcessInfoIn*)dioc.Type3InputBuffer;
+        //todo - open process and pass handle to caller
+        KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "INFO  ProcessProtect.sys: %s pid=%d\r\n", 
+            (dioc.IoControlCode == IOCTL_PROCESS_PROTECT_BY_PID)?"adding":"removing",
+            dataIn->pid));
 
-    //switch (dioc.IoControlCode)
-    //{
+        CLIENT_ID process{   };
+        auto h = IntToPtr(dataIn->pid);
 
-    //    break;
-    //}
+        if(dioc.IoControlCode == IOCTL_PROCESS_PROTECT_BY_PID)
+            ntStatus = data->protect(h);
+        else
+            ntStatus = data->unprotect(h);
+
+    }
+        break;
+
+    case IOCTL_PROCESS_PROTECT_CLEAR :
+        ntStatus = data->clear();
+        break;
+    }
+
+
 
     Irp->IoStatus.Status = ntStatus;
 
     IoCompleteRequest(Irp, IO_NO_INCREMENT);
 
     return ntStatus;
+}
+
+OB_PREOP_CALLBACK_STATUS PobPreOperationCallback(
+    PVOID RegistrationContext,
+    POB_PRE_OPERATION_INFORMATION OperationInformation
+)
+{
+    auto data = (Data*)RegistrationContext;
+    if (data->IsProtectedProcess(OperationInformation))
+    {
+        switch (OperationInformation->Operation) {
+        case OB_OPERATION_HANDLE_CREATE:
+            OperationInformation->Parameters->CreateHandleInformation.DesiredAccess &= ~CB_PROCESS_TERMINATE;
+            break;
+
+        case OB_OPERATION_HANDLE_DUPLICATE:
+            OperationInformation->Parameters->DuplicateHandleInformation.DesiredAccess &= ~CB_PROCESS_TERMINATE;
+            break;
+        }
+    }
+
+    return OB_PREOP_SUCCESS;
 }
